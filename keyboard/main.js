656c4517d065e9f7c2d3ae5dfc9121022c933f70
chloelangston <chloealangston@gmail.com>
2015-07-06 23:17:24 -0700

    added frog graphic

--- keyboard/main.js
+++ keyboard/main.js
@@ -11,6 +11,11 @@ var balloon2 = document.querySelector('#balloon2');
 var layer1 = document.querySelector('#Layer_1');
 var layer2 = document.querySelector('#Layer_2');
 
+var frog_down = document.querySelector('.armsdown');
+var frog_up = document.querySelector('.armsup');
+
+frog_up.style.opacity = 0;
+
 
 body.addEventListener('keydown', function(evt) {
   if (rightCounter % 2 === 0){
@@ -22,6 +27,7 @@ body.addEventListener('keydown', function(evt) {
       console.log(rightCounter);
       score2.innerText = rightCounter;
 
+
     }
    }
   else if (rightCounter % 2 === 1){
@@ -32,6 +38,7 @@ body.addEventListener('keydown', function(evt) {
       layer2.style.width = rightCounter*3 + 100 + "px";
       console.log(rightCounter);
       score2.innerText = rightCounter;
+
     }
   }
 
@@ -43,6 +50,8 @@ body.addEventListener('keydown', function(evt) {
       layer1.style.width = leftCounter*3 + 100 + "px";
       console.log(leftCounter);
       score1.innerText = leftCounter;
+      frog_up.style.opacity = 0;
+      frog_down.style.opacity = 1;
     }
   }
   else if (leftCounter % 2 === 1) {
@@ -53,6 +62,8 @@ body.addEventListener('keydown', function(evt) {
       layer1.style.width = leftCounter*3 + 100 + "px";
       console.log(leftCounter);
       score1.innerText = leftCounter;
+      frog_up.style.opacity = 1;
+      frog_down.style.opacity = 0;
     }
   }
   balloon1.style.width = rightCounter;
